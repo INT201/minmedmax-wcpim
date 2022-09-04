e7b00738beced0c8c26069b36288a7608df1528e
wcpimpim <waristha.0204@mail.kmutt.ac.th>
2022-09-05 00:39:57 +0700

    mmm

--- minMedMax.js
+++ minMedMax.js
@@ -1,10 +1,6 @@
 const { template } = require('@babel/core')
 
-function minMedMax(num1, num2, num3) {
-  const numberr = [num1, num2, num3]
-  let minn = numberr[0]
-  let medd = numberr[0]
-  let maxx = numberr[0]
+
 
   function minMedMax(num1, num2, num3) {
     const numberr = [num1, num2, num3]
@@ -38,9 +34,9 @@ function minMedMax(num1, num2, num3) {
       medd = num2
   }
   }
-  return {min: minn, mid: medd, max: maxx};
+  return {min: minn, mid: medd, max: maxx}
   
   }
-}
+
 module.exports = minMedMax
 
