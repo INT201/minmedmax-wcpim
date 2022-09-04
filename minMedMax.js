cfb14bb0f0880ce479a201e4e99b9dcc18aca86a
wcpimpim <waristha.0204@mail.kmutt.ac.th>
2022-09-05 00:55:12 +0700

    mmm

--- minMedMax.js
+++ minMedMax.js
@@ -2,76 +2,41 @@ const { template } = require('@babel/core')
 
 
 
-//   function minMedMax(num1, num2, num3) {
-//     const numberr = [num1, num2, num3]
-//     let minn = numberr[0]
-//     let medd = numberr[0]
-//     let maxx = numberr[0]
+  function minMedMax(num1, num2, num3) {
+    const numberr = [num1, num2, num3]
+    let minn = numberr[0]
+    let medd = numberr[0]
+    let maxx = numberr[0]
   
-//   for(const value of numberr){
-//   if (value < minn) {
-//      minn = value
-//   }
-//   }
-//   for(const value of numberr){
-//     if ( value > maxx){
-//       maxx = value
-//     }
-//   }
-//   for(const value of numberr){
-//    if (value != maxx && value != minn) { 
-//   medd = value}
-  
-//   if(num1 === num2){
-//       medd = num1
-//   }
-  
-//   if(num1 === num3){
-//       medd = num1
-//   }
-  
-//   if(num2 === num3){
-//       medd = num2
-//   }
-//   }
-//   return {min: minn, mid: medd, max: maxx}
-  
-//   }
-
-// module.exports = minMedMax
-
-function minMedMax(num1, num2, num3) {
-  const numberr = [num1, num2, num3]
-  let minn = numberr[0]
-  let medd = numberr[0]
-  let maxx = numberr[0]
-
-  for (const value of numberr) {
-    if (value > minn) {
-      minn = value;
+  for(const value of numberr){
+  if (value < minn) {
+     minn = value
+  }
+  }
+  for(const value of numberr){
+    if ( value > maxx){
+      maxx = value
     }
   }
-  for (const value of numberr) {
-    if (value < maxx) {
-      maxx = value;
-    }
+  for(const value of numberr){
+   if (value != maxx && value != minn) { 
+  medd = value}
+  
+  if(num1 === num2){
+      medd = num1
   }
-  for (const value of numberr) {
-    if (value != minn && value != maxx) {
-      medd = value
-    }
-    if (num1 === num2) {
+  
+  if(num1 === num3){
       medd = num1
-    }
-
-    if (num1 === num3) {
-      medd = num1
-    }
-
-    if (num2 === num3) {
+  }
+  
+  if(num2 === num3){
       medd = num2
-    }
   }
-  return { min: minn, mid: medd, max: maxx }
+  }
+  return {min: minn, mid: medd, max: maxx}
   
-}
+  }
+
+module.exports = minMedMax
+
