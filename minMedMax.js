92cdf8533d6204ace54cf92ea2cc7f6e74f02b9f
wcpimpim <waristha.0204@mail.kmutt.ac.th>
2022-09-04 18:15:35 +0700

    mmm

--- minMedMax.js
+++ minMedMax.js
@@ -2,15 +2,22 @@ const { template } = require('@babel/core')
 
 function minMedMax(num1, num2, num3) {
   const numberr = {num1, num2, num3}
-  let minn = numberr{0}
-  let medd = numberr{0}
-  let maxx = numberr{0}
+  let minn = numberr[0]
+  let medd = numberr[0]
+  let maxx = numberr[0]
 
-function  minn{
+
 if (const numberr < minn) {
   return minn = numberr
 }
-}
+  if (const numberr > maxx){
+    return maxx = numberr
+  }
+
+  else {
+    return medd = numberr
+ }
+
 
 
 module.exports = minMedMax
